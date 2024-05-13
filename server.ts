35709c2681f279efd5c47650ea63e6c37ee96c5a
Artur C <acceler945@gmail.com>
2024-05-13 15:04:57 +0200

    Initial commit

---
+++ server.ts
@@ -0,0 +1,28 @@
+import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
+
+const app: Express = express();
+const port = 8000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, this is Express + TypeScript');
+});
+
+app.use(express.urlencoded({ extended: false })); // required to handle urlencoded requests
+app.use(express.json()); // required to handle form-data request
+app.use(cors()); // middleware to enable CORS requests
+
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, '/client/build')));
+
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/client/build/index.html'));
+// });
+
+app.use((req, res) => {
+  res.status(404).send('404 not found...');
+});
+
+app.listen(port, () => {
+  console.log(`[Server]: I am running at https://localhost:${port}`);
+});
