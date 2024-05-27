e8b3c213160264767ec694ca1b05570e09304307
Artur C <acceler945@gmail.com>
2024-05-27 15:22:26 +0200

    Update task DTO to accept id as uuid

--- validators/tasks/create-task.dto.ts
+++ validators/tasks/create-task.dto.ts
@@ -6,8 +6,8 @@ const createTaskDTO = z.object({
     .min(1, { message: 'Title is required' })
     .max(255, { message: 'Title cannot be longer than 255 characters' }),
   desc: z.string().optional(),
-  authorId: z.number().int().nonnegative({ message: 'Author ID must be a non-negative integer' }),
-  boardId: z.number().int().nonnegative({ message: 'Board ID must be a non-negative integer' }),
+  authorId: z.string().uuid({ message: 'Author ID must be a non-negative integer' }),
+  boardId: z.string().uuid({ message: 'Board ID must be a non-negative integer' }),
 });
 
 export default createTaskDTO;
