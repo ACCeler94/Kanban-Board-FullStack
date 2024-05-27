f8742936e0add7004ff3958125f93e8ea582da4d
Artur C <acceler945@gmail.com>
2024-05-27 19:16:57 +0200

    Add user routes

---
+++ routes/users.routes.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import Router from 'express-promise-router';
+import validateParams from '../validators/validateParams';
+import UsersController from '../controllers/users.controller';
+
+const router = Router();
+
+// GET requests
+// [TODO - delete this endpoint for production]
+router.route('/users').get(UsersController.getAll);
+
+router.route('/users/:userId').get(validateParams, UsersController.getById);
+
+router.route('/users/search').get(UsersController.findByEmail);
+
+// POST requests
+router.route('/users').post(UsersController.createUser);
