326979632de48078c00e6de10fdc984391cc777c
Artur C <acceler945@gmail.com>
2024-05-27 17:32:29 +0200

    Create basic Auth0 skeleton

--- server.ts
+++ server.ts
@@ -2,17 +2,45 @@ import express, { Express, Request, Response } from 'express';
 import cors from 'cors';
 import { boardsRoutes } from './routes/boards.routes';
 import { tasksRoutes } from './routes/tasks.routes';
+import { auth, requiresAuth } from 'express-openid-connect';
 
 const app: Express = express();
 const port = 8000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, this is Express + TypeScript');
-});
-
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH_SECRET,
+  baseURL: 'http://localhost:8000/',
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_DOMAIN,
+  routes: {
+    login: '/auth/login',
+    logout: '/auth/logout',
+    callback: '/auth/callback',
+  },
+  session: {
+    cookie: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+    },
+    rolling: true,
+    rollingDuration: 86400, // 1 day
+    absoluteDuration: 604800, // 7 days
+  },
+};
+app.use(auth(config));
 app.use(express.urlencoded({ extended: false })); // required to handle urlencoded requests
 app.use(express.json()); // required to handle form-data request
 app.use(cors()); // middleware to enable CORS requests
+
+app.get('/', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
 
 // endpoints
 app.use('/api', boardsRoutes);
