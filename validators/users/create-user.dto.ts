614107556094403ad9a6c9675b745cca6c9ae139
Artur C <acceler945@gmail.com>
2024-05-27 19:17:32 +0200

    Change create user dto to use bult in zod email validator

--- validators/users/create-user.dto.ts
+++ validators/users/create-user.dto.ts
@@ -1,13 +1,11 @@
 import { z } from 'zod';
 
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
 const createUserDTO = z.object({
   name: z
     .string()
     .min(1, { message: 'Name is required' })
     .max(36, { message: 'Name cannot be longer than 36 characters' }),
-  email: z.string().regex(emailRegex, 'Invalid email format'),
+  email: z.string().email('Invalid email format'),
 });
 
 export default createUserDTO;
