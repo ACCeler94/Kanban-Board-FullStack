fee709e5dbcf0c152a0b927395ed7bb96db49c74
Artur C <acceler945@gmail.com>
2024-05-30 15:20:54 +0200

    modify create-user.dto to make email optional

--- validators/users/create-user.dto.ts
+++ validators/users/create-user.dto.ts
@@ -5,7 +5,7 @@ const createUserDTO = z.object({
     .string()
     .min(1, { message: 'Name is required' })
     .max(36, { message: 'Name cannot be longer than 36 characters' }),
-  email: z.string().email('Invalid email format'),
+  email: z.string().email('Invalid email format').optional(),
 });
 
 export default createUserDTO;
