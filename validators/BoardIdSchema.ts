fd02a45ccd417c8be90b6b2830d984b5125106e4
Artur C <acceler945@gmail.com>
2024-06-04 20:09:36 +0200

    Change BoardIdSchema to include message if the boardId does not exist

--- validators/BoardIdSchema.ts
+++ validators/BoardIdSchema.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod';
 
 const BoardIdSchema = z.object({
-  boardId: z.string().uuid({ message: 'Board ID must be a valid UUID' }),
+  boardId: z.string({ message: 'Board ID is required' }).uuid('Board ID must be a valid UUID'),
 });
 
 export default BoardIdSchema;
