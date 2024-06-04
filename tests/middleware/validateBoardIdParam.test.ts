c53989060952c7de7bee20baee4d67e1d0b8b4f6
Artur C <acceler945@gmail.com>
2024-06-04 20:09:53 +0200

    Add tests for validateBoardIdParam

---
+++ tests/middleware/validateBoardIdParam.test.ts
@@ -0,0 +1,55 @@
+import { Request, Response, NextFunction } from 'express';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import validateBoardIdParam from '../../middleware/validateBoardIdParam';
+
+describe('validateBoardIdParam', () => {
+  let req: Partial<Request>;
+  let res: Partial<Response>;
+  let next: NextFunction;
+
+  beforeEach(() => {
+    req = {
+      params: {},
+    };
+
+    res = {
+      status: vi.fn().mockReturnThis(),
+      json: vi.fn(),
+    };
+
+    next = vi.fn() as unknown as NextFunction;
+  });
+
+  it('should call next if given valid boardId', () => {
+    req.params = {
+      boardId: '9e65954d-f440-4dd9-8388-2a7a91005bf1',
+    };
+
+    validateBoardIdParam(req as Request, res as Response, next);
+
+    expect(next).toHaveBeenCalled();
+  });
+
+  it('should return 400 status and invalid board id error if the boardId param does not exist', () => {
+    req.params = {};
+
+    validateBoardIdParam(req as Request, res as Response, next);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+    expect(res.json).toHaveBeenCalledWith({
+      errors: ['Board ID is required'],
+    });
+  });
+
+  it('should return 400 status and invalid board id error if the boardId param does not exist', () => {
+    req.params = { boardId: 'abc' };
+
+    validateBoardIdParam(req as Request, res as Response, next);
+
+    expect(res.status).toHaveBeenCalledWith(400);
+
+    expect(res.json).toHaveBeenCalledWith({
+      errors: ['Board ID must be a valid UUID'],
+    });
+  });
+});
