b90d4f5ba689bd0d2ba98034f5ea62843c671116
Artur Celer <acceler945@gmail.com>
2024-07-09 20:18:10 +0200

    Add QueryClientProvider and ReactQueryDevtools

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar/Navbar';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Routes>
         <Route path='/boards' element={<Navbar />} />
       </Routes>
+      <ReactQueryDevtools initialIsOpen={false} />
     </>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,14 +10,19 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { CssBaseline, ThemeProvider } from '@mui/material';
 import { lightTheme } from './theme/theme.ts';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={lightTheme}>
-        <CssBaseline />
-        <App />
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider theme={lightTheme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
