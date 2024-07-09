7f2208a5c29997045b73bccbad0f4f4935b9d705
Artur Celer <acceler945@gmail.com>
2024-07-09 18:51:58 +0200

    Add ThemeProvider to main.tsx, adjust Container styling

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,18 +2,22 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-// Supports weights 200-800
-import '@fontsource-variable/plus-jakarta-sans';
+import '@fontsource-variable/plus-jakarta-sans'; // Supports weights 200-800
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { lightTheme } from './theme/theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
