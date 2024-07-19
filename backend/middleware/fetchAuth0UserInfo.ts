1ab9b2534005be44d44a6f0c90472cf164cc2d67
Artur Celer <acceler945@gmail.com>
2024-07-19 20:48:11 +0200

    Add middleware to fetch aut0 user object and save it in session

---
+++ backend/middleware/fetchAuth0UserInfo.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+import axios from 'axios';
+import Auth0User from '../types/Auth0User';
+
+// fetch auth0 user object and save it in session
+const fetchAuth0UserInfo = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const accessToken = req.headers.authorization?.split(' ')[1];
+    if (!accessToken) {
+      return res.status(401).json({ message: 'Authorization token missing' });
+    }
+
+    const response = await axios.get('https://acceler945.eu.auth0.com/userinfo', {
+      headers: {
+        authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    const userData = response.data as Auth0User;
+    // basic check to make sure sub is provided
+    if (userData && typeof userData.sub === 'string') req.session.auth0User = userData; // Save user info to session
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default fetchAuth0UserInfo;
