bec4485afe8e3d843c6d53dd93d8a7c4e88b877b
Artur Celer <acceler945@gmail.com>
2024-07-23 20:02:43 +0200

    Add PostLoginPage to handle post auth0 login endpoint

---
+++ client/src/pages/PostLoginPage/PostLoginPage.tsx
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { authUrl } from '../../API/config';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
+
+const PostLoginPage = () => {
+  const { getAccessTokenSilently, isLoading, isAuthenticated } = useAuth0();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handlePostLogin = async () => {
+      try {
+        const token = await getAccessTokenSilently();
+        await axios.get(`${authUrl}/post-login`, {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+          withCredentials: true,
+        });
+
+        navigate('/boards');
+      } catch (error) {
+        // [TODO - change to logout and ErrorPage redirect]
+        console.log('Login failed');
+        console.log(error);
+      }
+    };
+
+    if (!isLoading) handlePostLogin();
+  }, [isLoading, isAuthenticated, getAccessTokenSilently, navigate]);
+
+  return <div>Processing login...</div>;
+};
+
+export default PostLoginPage;
