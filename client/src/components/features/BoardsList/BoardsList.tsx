bfd68d44e48cda36616db1588a4b4de86ad7d8de
Artur Celer <acceler945@gmail.com>
2024-08-21 14:46:55 +0200

    Add activeBoard mock and styling

--- client/src/components/features/BoardsList/BoardsList.tsx
+++ client/src/components/features/BoardsList/BoardsList.tsx
@@ -1,3 +1,5 @@
+import styles from './BoardsList.module.css';
+
 interface Board {
   board: {
     title: string;
@@ -10,17 +12,28 @@ interface BoardsListProps {
 }
 
 const BoardsList = ({ boards }: BoardsListProps) => {
+  const activeBoard = { id: '1e5ca4e7-2f18-4901-a30e-696746693ed1', title: 'Board One' }; //  mock value [TODO - change to take from zustand store]
+
   return (
-    <>
-      <div>Your Boards</div>
-      <ul>
-        {boards.map((boardObj, index) => (
-          <li key={index} board-id={boardObj.board.id}>
-            {boardObj.board.title}
-          </li>
-        ))}
+    <div>
+      <h2 className={styles.boardsListHeader}>All Boards ({boards.length})</h2>
+      <ul className={styles.boardsList}>
+        {boards.map((boardObj, index) => {
+          const isActive = activeBoard && boardObj.board.id === activeBoard.id;
+          return (
+            <li
+              key={index}
+              board-id={boardObj.board.id}
+              className={
+                isActive ? `${styles.active} ${styles.boardsListItem}` : styles.boardsListItem
+              }
+            >
+              {boardObj.board.title}
+            </li>
+          );
+        })}
       </ul>
-    </>
+    </div>
   );
 };
 
