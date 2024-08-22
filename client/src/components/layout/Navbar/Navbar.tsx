dde6357a507e0ee33610829c9804bc7344b67bcf
Artur Celer <acceler945@gmail.com>
2024-08-22 17:08:20 +0200

    Add dynamic header based on selected board

--- client/src/components/layout/Navbar/Navbar.tsx
+++ client/src/components/layout/Navbar/Navbar.tsx
@@ -2,20 +2,22 @@ import { Button } from '@mui/material';
 import Icon from '../../../assets/icon.svg?react';
 import Container from '../../common/Container/Container';
 import styles from './Navbar.module.css';
-import { Link } from 'react-router-dom';
 import { FaPlus } from 'react-icons/fa';
+import useStore from '../../../store/useStore';
 
 const Navbar = () => {
+  const activeBoard = useStore((state) => state.activeBoard);
+
   return (
     <nav className={styles.navbar}>
       <Container>
         <div className={styles.wrapper}>
-          <Link to='/boards' className={styles.titleContainer}>
+          <div className={styles.titleContainer}>
             <Icon />
             <h1 className={styles.title}>kanban</h1>
-          </Link>
+          </div>
           <div className={styles.utilityBar}>
-            <h2>Select Board...</h2>
+            <h2>{activeBoard?.title || 'Select a board...'}</h2>
             <Button color='primary' variant='contained' className='button-small' disabled>
               <FaPlus />
               <span>Add New Task</span>
