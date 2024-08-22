21c1a94dec3e6287311263e8303fef25722e91d4
Artur Celer <acceler945@gmail.com>
2024-08-22 17:56:28 +0200

    Add active board selection in BoardsList, adjust styling

--- client/src/components/features/BoardsList/BoardsList.tsx
+++ client/src/components/features/BoardsList/BoardsList.tsx
@@ -1,5 +1,8 @@
 import styles from './BoardsList.module.css';
 import { FaPlus } from 'react-icons/fa';
+import useStore from '../../../store/useStore';
+import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
 
 interface Board {
   board: {
@@ -13,7 +16,13 @@ interface BoardsListProps {
 }
 
 const BoardsList = ({ boards }: BoardsListProps) => {
-  const activeBoard = { id: '1e5ca4e7-2f18-4901-a30e-696746693ed1', title: 'Board One' }; //  mock value [TODO - change to take from zustand store]
+  const activeBoard = useStore((state) => state.activeBoard);
+  const setActiveBoard = useStore((state) => state.setActiveBoard);
+
+  // reset activeBoard on mount
+  useEffect(() => {
+    setActiveBoard(null);
+  }, [setActiveBoard]);
 
   return (
     <div>
@@ -22,20 +31,24 @@ const BoardsList = ({ boards }: BoardsListProps) => {
         {boards.map((boardObj, index) => {
           const isActive = activeBoard && boardObj.board.id === activeBoard.id;
           return (
-            <li
-              key={index}
-              board-id={boardObj.board.id}
-              className={
-                isActive ? `${styles.active} ${styles.boardsListItem}` : styles.boardsListItem
-              }
-            >
-              {boardObj.board.title}
+            <li key={index} board-id={boardObj.board.id}>
+              <Link
+                to={boardObj.board.id}
+                className={isActive ? `${styles.active} ${styles.boardItem}` : styles.boardItem}
+                onClick={() =>
+                  setActiveBoard({ id: boardObj.board.id, title: boardObj.board.title })
+                }
+              >
+                {boardObj.board.title}
+              </Link>
             </li>
           );
         })}
-        <li className={styles.addBoardButton}>
-          <FaPlus />
-          <span>Create new board</span>
+        <li>
+          <Link to='add' className={styles.addBoardButton}>
+            <FaPlus />
+            <span>Create new board</span>
+          </Link>
         </li>
       </ul>
     </div>
