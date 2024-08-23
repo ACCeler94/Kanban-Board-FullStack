68bf97c7e451b414df14aef331f0daccc315ad04
Artur Celer <acceler945@gmail.com>
2024-08-23 14:27:47 +0200

    Improve fetchBoardById error handling, add stale time and enabled property to prevent calling queryFn without proper id

--- client/src/API/boards.ts
+++ client/src/API/boards.ts
@@ -1,23 +1,34 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { apiUrl } from './config';
+import { validate as uuidValidate } from 'uuid';
 
 // actions
-const fetchBoardById = async (id: string) => {
+const fetchBoardById = async (id: string | undefined) => {
+  if (!id) {
+    // check if id exists and is valid uuid
+    throw new Error('Invalid Board ID.');
+  }
+
   const { data } = await axios.get(`${apiUrl}/boards/${id}`);
 
   return data;
 };
 
 // hooks
-const useBoardById = (id: string) => {
+const useBoardById = (id: string | undefined) => {
   const {
-    data: board,
+    data: boardData,
     error,
     isPending,
-  } = useQuery({ queryKey: ['board', id], queryFn: () => fetchBoardById(id) });
+  } = useQuery({
+    queryKey: ['board', id],
+    queryFn: () => fetchBoardById(id),
+    enabled: !!id && uuidValidate(id), // check for the existence of the id and if it's valid uuid to prevent unnecessary calls to fetchBoardById
+    staleTime: 2 * 60 * 1000, // 2 minutes
+  });
 
-  return { board, error, isPending };
+  return { boardData, error, isPending };
 };
 
 export { useBoardById };
