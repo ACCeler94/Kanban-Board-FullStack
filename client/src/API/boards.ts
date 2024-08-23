7c80b4777669d24ad70634ece3385a7387213c5d
Artur Celer <acceler945@gmail.com>
2024-08-23 14:36:50 +0200

    Add authorization token to fetchBoardById query function

--- client/src/API/boards.ts
+++ client/src/API/boards.ts
@@ -2,29 +2,53 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { apiUrl } from './config';
 import { validate as uuidValidate } from 'uuid';
+import { useAuth0 } from '@auth0/auth0-react';
 
 // actions
-const fetchBoardById = async (id: string | undefined) => {
+const fetchBoardById = async (id: string | undefined, token: string) => {
   if (!id) {
     // check if id exists and is valid uuid
     throw new Error('Invalid Board ID.');
   }
 
-  const { data } = await axios.get(`${apiUrl}/boards/${id}`);
-
-  return data;
+  try {
+    const { data } = await axios.get(`${apiUrl}/boards/${id}`, {
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+      withCredentials: true,
+    });
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(
+        `Failed to fetch user data: ${error.response?.status} ${error.response?.statusText}`
+      );
+    } else {
+      throw new Error('An unexpected error occurred');
+    }
+  }
 };
 
 // hooks
 const useBoardById = (id: string | undefined) => {
+  const { getAccessTokenSilently, isAuthenticated } = useAuth0();
+
   const {
     data: boardData,
     error,
     isPending,
   } = useQuery({
     queryKey: ['board', id],
-    queryFn: () => fetchBoardById(id),
-    enabled: !!id && uuidValidate(id), // check for the existence of the id and if it's valid uuid to prevent unnecessary calls to fetchBoardById
+    queryFn: async () => {
+      try {
+        const token = await getAccessTokenSilently();
+        return fetchBoardById(id, token);
+      } catch (error) {
+        throw new Error('Failed to authenticate. Please try logging in again.');
+      }
+    },
+    enabled: !!id && uuidValidate(id) && isAuthenticated, // check for the existence of the id and if it's valid uuid to prevent unnecessary calls to fetchBoardById
     staleTime: 2 * 60 * 1000, // 2 minutes
   });
 
