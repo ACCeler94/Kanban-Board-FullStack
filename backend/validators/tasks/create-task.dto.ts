e5fc8786e942246df9ee104bc03238ee4a3cd20d
Artur Celer <acceler945@gmail.com>
2024-08-29 16:54:40 +0200

    Update create-task.dto and edit-task.dto to allow subtask data

--- backend/validators/tasks/create-task.dto.ts
+++ backend/validators/tasks/create-task.dto.ts
@@ -1,12 +1,23 @@
 import { z } from 'zod';
 
 const createTaskDTO = z.object({
-  title: z
-    .string()
-    .min(1, { message: 'Title is required' })
-    .max(100, { message: 'Title cannot be longer than 100 characters' }),
-  desc: z.string().optional(),
-  boardId: z.string().uuid({ message: 'Board ID must be a non-negative integer' }),
+  taskData: z.object({
+    title: z
+      .string()
+      .min(1, { message: 'Title is required' })
+      .max(100, { message: 'Title cannot be longer than 100 characters' }),
+    desc: z.string().optional(),
+    boardId: z.string().uuid({ message: 'Board ID must be a non-negative integer' }),
+  }),
+  subtaskData: z
+    .object({
+      desc: z
+        .string()
+        .min(1, { message: 'Description must be at least 1 character' })
+        .max(100, { message: 'Description cannot be longer than 280 characters' }),
+    })
+    .array()
+    .optional(),
 });
 
 export default createTaskDTO;
--- backend/validators/tasks/edit-task.dto.ts
+++ backend/validators/tasks/edit-task.dto.ts
@@ -2,18 +2,28 @@ import { z } from 'zod';
 
 const TaskStatus = z.enum(['TO_DO', 'IN_PROGRESS', 'DONE']);
 
-const editTaskDTO = z.object({
-  title: z
-    .string()
-    .min(1, { message: 'Title must be at least 1 character' })
-    .max(100, { message: 'Title cannot be longer than 100 characters' })
+export const editTaskDTO = z.object({
+  taskData: z.object({
+    title: z
+      .string()
+      .min(1, { message: 'Title must be at least 1 character' })
+      .max(100, { message: 'Title cannot be longer than 100 characters' })
+      .optional(),
+    desc: z
+      .string()
+      .min(1, { message: 'Title must be at least 1 character' })
+      .max(1000, { message: 'Description cannot be longer than 1000 characters' })
+      .optional(),
+    status: TaskStatus.optional(),
+  }),
+  subtaskData: z
+    .object({
+      id: z.string().uuid().optional(),
+      desc: z
+        .string()
+        .min(1, { message: 'Description must be at least 1 character' })
+        .max(100, { message: 'Description cannot be longer than 280 characters' }),
+    })
+    .array()
     .optional(),
-  desc: z
-    .string()
-    .min(1, { message: 'Title must be at least 1 character' })
-    .max(1000, { message: 'Description cannot be longer than 1000 characters' })
-    .optional(),
-  status: TaskStatus.optional(),
 });
-
-export default editTaskDTO;
