cbb87e9f6650a5d33fada188e2119fa2e7f725c6
Artur Celer <acceler945@gmail.com>
2024-08-31 17:45:15 +0200

    Add indexes to Board, Task and Subtask schemas

---
+++ backend/prisma/migrations/20240831152022_add_indexes/migration.sql
@@ -0,0 +1,8 @@
+-- CreateIndex
+CREATE INDEX `Board_createdAt_idx` ON `Board`(`createdAt`);
+
+-- CreateIndex
+CREATE INDEX `Subtask_createdAt_idx` ON `Subtask`(`createdAt`);
+
+-- CreateIndex
+CREATE INDEX `Task_createdAt_idx` ON `Task`(`createdAt`);
