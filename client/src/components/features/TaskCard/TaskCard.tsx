d7bda8e9af1ca320a69d34cd09d254abe904dced
Artur Celer <acceler945@gmail.com>
2024-09-05 21:33:37 +0200

    Add TaskModal to TaskCard, adjust styling

--- client/src/components/features/TaskCard/TaskCard.tsx
+++ client/src/components/features/TaskCard/TaskCard.tsx
@@ -1,13 +1,21 @@
 import { useEffect, useState } from 'react';
-import { TaskType } from '../../../types/types';
+import { TaskTypePartial } from '../../../types/types';
+import TaskModal from '../TaskModal/TaskModal';
 import styles from './TaskCard.module.css';
 
 interface TaskCardProps {
-  taskData: TaskType;
+  taskData: TaskTypePartial;
 }
 
 const TaskCard = ({ taskData }: TaskCardProps) => {
   const [finishedSubtasksCount, setFinishedSubtasksCount] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = (event: object, reason: string) => {
+    if (reason === 'escapeKeyDown' || reason === 'backdropClick') return;
+
+    setIsOpen(false);
+  };
 
   useEffect(() => {
     if (taskData.subtasks.length !== 0) {
@@ -20,14 +28,27 @@ const TaskCard = ({ taskData }: TaskCardProps) => {
   }, [taskData.subtasks]);
 
   return (
-    <div className={styles.taskCard}>
-      <h3>{taskData.title}</h3>
-      <p>
-        {taskData.subtasks.length !== 0
-          ? `${finishedSubtasksCount} of ${taskData.subtasks.length} subtasks`
-          : 'No subtasks'}
-      </p>
-    </div>
+    <>
+      <div className={styles.taskCard} onClick={() => setIsOpen(true)}>
+        <h3>{taskData.title}</h3>
+        <p>
+          {taskData.subtasks.length !== 0
+            ? `${finishedSubtasksCount} of ${taskData.subtasks.length} subtasks`
+            : 'No subtasks'}
+        </p>
+      </div>
+      {
+        // conditionally render modal to avoid unnecessary task data fetching - it fetches task data on mount
+        isOpen ? (
+          <TaskModal
+            isOpen={isOpen}
+            handleClose={handleClose}
+            setIsOpen={setIsOpen}
+            taskId={taskData.id}
+          />
+        ) : null
+      }
+    </>
   );
 };
 
