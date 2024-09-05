41c9d45576d6335983b5e6b6b30078471c00aedb
Artur Celer <acceler945@gmail.com>
2024-09-05 21:34:01 +0200

    Add TaskMenu component with styling

---
+++ client/src/components/features/TaskMenu/TaskMenu.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { Link } from 'react-router-dom';
+import { HiDotsVertical } from 'react-icons/hi';
+import styles from './TaskMenu.module.css';
+import { Divider } from '@mui/material';
+
+const TaskMenu = () => {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <button
+        id='task-menu-button'
+        aria-controls={open ? 'task-menu-button' : undefined}
+        aria-haspopup='true'
+        aria-expanded={open ? 'true' : undefined}
+        onClick={handleClick}
+        className={styles.menuButton}
+      >
+        <HiDotsVertical />
+      </button>
+      <Menu
+        id='task-menu'
+        aria-labelledby='task-menu'
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Link to='edit'>Edit</Link>
+        </MenuItem>
+        <Divider sx={{ my: 0.5 }} />
+        <MenuItem onClick={handleClose} sx={{ color: 'var(--red)', fontWeight: '700' }}>
+          Delete
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+};
+
+export default TaskMenu;
