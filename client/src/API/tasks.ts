98415f2b1c6ac71156416dc119142fbea5e1b5f9
Artur Celer <acceler945@gmail.com>
2024-09-18 17:47:53 +0200

    Add editTaskById function and useEditTask mutation hook

--- client/src/API/tasks.ts
+++ client/src/API/tasks.ts
@@ -3,7 +3,7 @@ import { apiUrl } from './config';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { validate as uuidValidate } from 'uuid';
-import { TaskType } from '../types/types';
+import { EditTaskData, TaskType } from '../types/types';
 
 const fetchTaskById = async (taskId: string, token: string): Promise<TaskType | undefined> => {
   if (!taskId) throw new Error('Invalid Task ID.');
@@ -23,7 +23,7 @@ const fetchTaskById = async (taskId: string, token: string): Promise<TaskType | 
         `Failed to fetch task data: ${error.response?.status} ${error.response?.statusText}`
       );
     } else {
-      throw new Error('An unexpected error occurred');
+      throw new Error('An unexpected error occurred.');
     }
   }
 };
@@ -33,7 +33,7 @@ const deleteTaskById = async (taskId: string, token: string) => {
   try {
     const { data } = await axios.delete(`${apiUrl}/tasks/${taskId}`, {
       headers: {
-        authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${token}`,
       },
       withCredentials: true,
     });
@@ -45,7 +45,42 @@ const deleteTaskById = async (taskId: string, token: string) => {
         `Failed to fetch task data: ${error.response?.status} ${error.response?.statusText}`
       );
     } else {
-      throw new Error('An unexpected error occurred');
+      throw new Error('An unexpected error occurred.');
+    }
+  }
+};
+
+const editTaskById = async (
+  { taskData, subtaskData }: EditTaskData,
+  taskId: string,
+  token: string
+) => {
+  if (
+    (!taskData || Object.keys(taskData).length === 0) &&
+    (!subtaskData || subtaskData.length === 0)
+  ) {
+    throw new Error('No data to edit provided.');
+  }
+  try {
+    const { data } = await axios.patch(
+      `${apiUrl}/tasks/${taskId}`,
+      { taskData, subtaskData }, // body obj containing optional task and subtask data
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true,
+      }
+    );
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(
+        `Failed to update task data: ${error.response?.status} ${error.response?.statusText}`
+      );
+    } else {
+      throw new Error('An unexpected error occurred.');
     }
   }
 };
@@ -76,6 +111,8 @@ const useDeleteTask = (taskId: string, boardId: string) => {
 
   const { mutate, data, error, isPending } = useMutation({
     mutationFn: async () => {
+      if (!taskId || !uuidValidate(taskId)) throw new Error('Invalid task ID.');
+
       try {
         const token = await getAccessTokenSilently();
         return deleteTaskById(taskId, token);
@@ -95,4 +132,28 @@ const useDeleteTask = (taskId: string, boardId: string) => {
   return { mutate, data, error, isPending };
 };
 
-export { useTaskData, useDeleteTask };
+const useEditTask = (taskId: string, editData: EditTaskData) => {
+  const { getAccessTokenSilently } = useAuth0();
+  const queryClient = useQueryClient();
+
+  const { mutate, data, error, isPending } = useMutation({
+    mutationFn: async () => {
+      if (!taskId || !uuidValidate(taskId)) throw new Error('Invalid task ID.');
+
+      try {
+        const token = await getAccessTokenSilently();
+        return editTaskById(editData, taskId, token);
+      } catch (error) {
+        throw new Error('Failed to authenticate. Please try logging in again.');
+      }
+    },
+    onSuccess: (updatedTask: TaskType) => {
+      queryClient.setQueryData(['task', taskId], updatedTask);
+      queryClient.invalidateQueries({ queryKey: ['board', updatedTask.boardId] }); // board invalidation done to properly update subtask counter in the TaskCard
+    },
+  });
+
+  return { mutate, data, error, isPending };
+};
+
+export { useTaskData, useDeleteTask, useEditTask };
