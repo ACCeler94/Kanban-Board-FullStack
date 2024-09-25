a535794c0d89b4bce35b6c162aa538d1d8e85429
Artur Celer <acceler945@gmail.com>
2024-09-25 11:09:05 +0200

    Add validator to useEditTask, add useCreateTask hook, improve error handling in other hooks

--- client/src/API/tasks.ts
+++ client/src/API/tasks.ts
@@ -3,7 +3,9 @@ import { apiUrl } from './config';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { validate as uuidValidate } from 'uuid';
-import { EditTaskData, TaskType } from '../types/types';
+import { BoardType, EditTaskData, NewTaskData, TaskType } from '../types/types';
+import editTaskValidator from '../validators/tasks/editTaskValidator';
+import addTaskValidator from '../validators/tasks/addTaskValidator';
 
 const fetchTaskById = async (taskId: string, token: string): Promise<TaskType | undefined> => {
   if (!taskId) throw new Error('Invalid Task ID.');
@@ -55,12 +57,6 @@ const editTaskById = async (
   taskId: string,
   token: string
 ) => {
-  if (
-    (!taskData || Object.keys(taskData).length === 0) &&
-    (!subtaskData || subtaskData.length === 0)
-  ) {
-    throw new Error('No data to edit provided.');
-  }
   try {
     const { data } = await axios.patch(
       `${apiUrl}/tasks/${taskId}`,
@@ -85,17 +81,47 @@ const editTaskById = async (
   }
 };
 
+const addNewTask = async ({ taskData, subtaskData }: NewTaskData, token: string) => {
+  try {
+    const { data } = await axios.post(
+      `${apiUrl}/tasks`,
+      { taskData, subtaskData },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        withCredentials: true,
+      }
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(
+        `Failed to create task: ${error.response?.status} ${error.response?.statusText}`
+      );
+    } else {
+      throw new Error('An unexpected error occurred.');
+    }
+  }
+};
+
 const useTaskData = (taskId: string) => {
   const { getAccessTokenSilently, isAuthenticated } = useAuth0();
 
   const { data, error, isPending, refetch } = useQuery({
     queryKey: ['task', taskId],
     queryFn: async () => {
+      let token;
       try {
-        const token = await getAccessTokenSilently();
-        return fetchTaskById(taskId, token);
+        token = await getAccessTokenSilently();
       } catch (error) {
         throw new Error('Failed to authenticate. Please try logging in again.');
+      }
+
+      try {
+        return fetchTaskById(taskId, token);
+      } catch (error) {
+        throw new Error('Failed to fetch the task data. Please try again.');
       }
     },
     enabled: !!taskId && uuidValidate(taskId) && isAuthenticated, // check for the existence of the id and if it's valid uuid to prevent unnecessary calls to fetchTaskById
@@ -113,11 +139,17 @@ const useDeleteTask = (taskId: string, boardId: string) => {
     mutationFn: async () => {
       if (!taskId || !uuidValidate(taskId)) throw new Error('Invalid task ID.');
 
+      let token;
       try {
-        const token = await getAccessTokenSilently();
-        return deleteTaskById(taskId, token);
+        token = await getAccessTokenSilently();
       } catch (error) {
         throw new Error('Failed to authenticate. Please try logging in again.');
+      }
+
+      try {
+        return deleteTaskById(taskId, token);
+      } catch (error) {
+        throw new Error('Failed to delete the task. Please try again.');
       }
     },
     onSuccess: () => {
@@ -139,12 +171,21 @@ const useEditTask = (taskId: string, editData: EditTaskData) => {
   const { mutate, data, error, isPending } = useMutation({
     mutationFn: async () => {
       if (!taskId || !uuidValidate(taskId)) throw new Error('Invalid task ID.');
+      const validationResult = editTaskValidator.safeParse(editData); // data validation with zod
+      if (!validationResult.success)
+        throw new Error('Invalid data: ' + validationResult.error.message);
 
+      let token;
       try {
-        const token = await getAccessTokenSilently();
-        return editTaskById(editData, taskId, token);
+        token = await getAccessTokenSilently();
       } catch (error) {
         throw new Error('Failed to authenticate. Please try logging in again.');
+      }
+
+      try {
+        return editTaskById(editData, taskId, token);
+      } catch (error) {
+        throw new Error('Failed to edit the task. Please try again.');
       }
     },
     onSuccess: (updatedTask: TaskType) => {
@@ -156,4 +197,38 @@ const useEditTask = (taskId: string, editData: EditTaskData) => {
   return { mutate, data, error, isPending };
 };
 
-export { useTaskData, useDeleteTask, useEditTask };
+const useCreateTask = (taskData: NewTaskData) => {
+  const { getAccessTokenSilently } = useAuth0();
+  const queryClient = useQueryClient();
+
+  const { mutate, data, error, isPending } = useMutation({
+    mutationFn: async () => {
+      const validationResult = addTaskValidator.safeParse(taskData);
+      if (!validationResult.success)
+        throw new Error('Invalid task data: ' + validationResult.error.message);
+
+      let token;
+      try {
+        token = await getAccessTokenSilently();
+      } catch (error) {
+        throw new Error('Failed to authenticate. Please try logging in again.');
+      }
+
+      try {
+        return addNewTask(taskData, token);
+      } catch (error) {
+        throw new Error('Failed to create a task. Please try again.');
+      }
+    },
+    onSuccess: (createdTask: TaskType) => {
+      queryClient.setQueryData(['board', createdTask.boardId], (oldData: BoardType) => {
+        if (!oldData) return; // if there is no old board data return
+
+        return { ...oldData, tasks: [...oldData.tasks, createdTask] };
+      });
+    },
+  });
+  return { mutate, data, isPending, error };
+};
+
+export { useTaskData, useDeleteTask, useEditTask, useCreateTask };
