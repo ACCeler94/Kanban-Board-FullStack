195b5dd42ffc3d608e1118d0f90835fbb5326a50
Artur Celer <acceler945@gmail.com>
2024-09-27 17:59:15 +0200

    Update SubtasksInputs to generate id for new subtasks and utilize in during updates

--- client/src/components/common/SubtasksInputs/SubtasksInputs.tsx
+++ client/src/components/common/SubtasksInputs/SubtasksInputs.tsx
@@ -1,9 +1,10 @@
 import { List, ListItem, TextField } from '@mui/material';
 import { useState } from 'react';
 import { IoAddCircle } from 'react-icons/io5';
+import { MdOutlineDeleteForever } from 'react-icons/md';
+import { v4 as uuid } from 'uuid';
 import { NewSubtaskData, Subtask } from '../../../types/types';
 import styles from './SubtasksInputs.module.css';
-import { MdOutlineDeleteForever } from 'react-icons/md';
 
 interface SubtasksInputsProps {
   subtasks: (Subtask | NewSubtaskData)[];
@@ -11,24 +12,30 @@ interface SubtasksInputsProps {
 }
 
 const SubtasksInputs = ({ subtasks, setSubtasks }: SubtasksInputsProps) => {
-  const [newSubtask, setNewSubtask] = useState<NewSubtaskData>({ desc: '', finished: false });
+  const [newSubtask, setNewSubtask] = useState<NewSubtaskData>({
+    id: '',
+    desc: '',
+    finished: false,
+  });
 
   const handleSubtaskChange =
-    (index: number) => (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-      const updatedSubtasks = subtasks.map((subtask, i) =>
-        i === index ? { ...subtask, desc: event.target.value } : subtask
+    (id: string) => (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+      const updatedSubtasks = subtasks.map((subtask) =>
+        subtask.id === id ? { ...subtask, desc: event.target.value } : subtask
       );
       setSubtasks(updatedSubtasks);
     };
 
   const addSubtask = () => {
     if (newSubtask.desc.length === 0) return; // prevent adding empty subtasks
-    setSubtasks([...subtasks, newSubtask]);
-    setNewSubtask({ desc: '', finished: false });
+    const subtaskToAdd = { ...newSubtask, id: uuid() }; // add unique id
+
+    setSubtasks([...subtasks, subtaskToAdd]);
+    setNewSubtask({ id: '', desc: '', finished: false });
   };
 
-  const handleSubtaskDelete = (index: number) => {
-    const updatedSubtasks = subtasks.filter((_, i) => i !== index);
+  const handleSubtaskDelete = (id: string) => {
+    const updatedSubtasks = subtasks.filter((subtask) => id !== subtask.id);
     setSubtasks(updatedSubtasks);
   };
 
@@ -36,16 +43,12 @@ const SubtasksInputs = ({ subtasks, setSubtasks }: SubtasksInputsProps) => {
     <div className={styles.subtasksWrapper}>
       <span className={styles.inputsLabel}>Subtasks</span>
       <List>
-        {subtasks.map((subtask, index) => (
-          <ListItem
-            className={styles.inputWrapper}
-            sx={{ padding: 0 }}
-            key={`edit-${index}-${subtask.desc}`}
-          >
+        {subtasks.map((subtask) => (
+          <ListItem className={styles.inputWrapper} sx={{ padding: 0 }} key={subtask.id}>
             <TextField
-              aria-label={`Edit Subtask ${index + 1}`}
+              aria-label={`Edit Subtask ${subtask.id}`}
               value={subtask.desc}
-              onChange={handleSubtaskChange(index)}
+              onChange={handleSubtaskChange(subtask.id)}
               sx={{ marginRight: '10px' }}
               fullWidth
               required
@@ -55,7 +58,7 @@ const SubtasksInputs = ({ subtasks, setSubtasks }: SubtasksInputsProps) => {
               className={styles.deleteButton}
               type='button'
               aria-label='Delete Subtask'
-              onClick={() => handleSubtaskDelete(index)}
+              onClick={() => handleSubtaskDelete(subtask.id)}
             >
               <MdOutlineDeleteForever />
             </button>
@@ -68,7 +71,7 @@ const SubtasksInputs = ({ subtasks, setSubtasks }: SubtasksInputsProps) => {
             placeholder='Break down this task into smaller steps...'
             sx={{ marginRight: '10px' }}
             value={newSubtask.desc}
-            onChange={(event) => setNewSubtask({ desc: event.target.value, finished: false })}
+            onChange={(event) => setNewSubtask({ ...newSubtask, desc: event.target.value })}
           />
           <button
             className={styles.addButton}
