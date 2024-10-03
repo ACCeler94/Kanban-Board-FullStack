ea25eb3e4c0b388327e104a4f255fbc9aa0fbd83
Artur Celer <acceler945@gmail.com>
2024-10-03 12:48:52 +0200

    Add route to display EditTaskModal

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import PostLoginPage from './pages/PostLoginPage/PostLoginPage';
 import Board from './components/features/Board/Board';
 import AddTaskModal from './components/features/AddTaskModal/AddTaskModal';
 import TaskModal from './components/features/TaskModal/TaskModal';
+import EditTaskModal from './components/features/EditTaskModal/EditTaskModal';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path=':id' element={<Board />}>
             <Route path='tasks/add-task' element={<AddTaskModal />} />
             <Route path='tasks/:taskId' element={<TaskModal />} />
+            <Route path='tasks/:taskId/edit' element={<EditTaskModal />} />
           </Route>
         </Route>
         <Route path='/post-login' element={<PostLoginPage />} />
