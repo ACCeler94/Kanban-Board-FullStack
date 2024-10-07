fa4a7efd4f735caa98847d57a3444053bcbb643f
Artur Celer <acceler945@gmail.com>
2024-10-07 18:08:32 +0200

    Add BoardForm component and BoardForm.module.css

---
+++ client/src/components/common/BoardForm/BoardForm.tsx
@@ -0,0 +1,55 @@
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { useState } from 'react';
+import styles from './BoardForm.module.css';
+
+interface BoardFormProps {
+  boardTitle?: string;
+  submitHandler: (title: string) => void;
+  buttonText: string;
+}
+
+const BoardForm = ({ boardTitle, submitHandler, buttonText }: BoardFormProps) => {
+  const [title, setTitle] = useState(boardTitle || '');
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    submitHandler(title);
+  };
+
+  return (
+    <form autoComplete='off' onSubmit={handleSubmit}>
+      <div className={styles.inputWrapper}>
+        <label htmlFor='title'>Board Title</label>
+        <TextField
+          id='board-title'
+          variant='outlined'
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          fullWidth
+          sx={{ marginTop: '5px' }}
+          placeholder='Title should be short and allow to easily identify the board.'
+          required
+          inputProps={{ maxLength: 255 }}
+          onKeyDown={(e: React.KeyboardEvent<HTMLInputElement>) => {
+            e.key === 'Enter' && e.preventDefault();
+          }}
+        />
+      </div>
+
+      <Button
+        type='submit'
+        color='primary'
+        variant='contained'
+        className='button-small'
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') e.preventDefault();
+        }}
+      >
+        {buttonText}
+      </Button>
+    </form>
+  );
+};
+
+export default BoardForm;
