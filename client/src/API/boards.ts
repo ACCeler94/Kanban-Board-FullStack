d769b33977442633482376b47fe549ba799c34a4
Artur Celer <acceler945@gmail.com>
2024-10-14 20:55:50 +0200

    Add deleteBoardById mutation fn, add useDeleteBoard hook

--- client/src/API/boards.ts
+++ client/src/API/boards.ts
@@ -89,6 +89,28 @@ const editBoard = async (title: string, boardId: string, token: string) => {
   }
 };
 
+const deleteBoardById = async (boardId: string, token: string) => {
+  if (!boardId) throw new Error('Invalid Task ID.');
+  try {
+    const { data } = await axios.delete(`${apiUrl}/boards/${boardId}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      withCredentials: true,
+    });
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(
+        `Failed to fetch task data: ${error.response?.status} ${error.response?.statusText}`
+      );
+    } else {
+      throw new Error('An unexpected error occurred.');
+    }
+  }
+};
+
 // Hooks
 const useBoardById = (id: string | undefined) => {
   const { getAccessTokenSilently, isAuthenticated } = useAuth0();
@@ -189,4 +211,34 @@ const useEditBoard = (id: string) => {
   return { mutate, data, error, isPending, isSuccess };
 };
 
-export { useBoardById, useCreateBoard, useEditBoard };
+const useDeleteBoard = (boardId: string) => {
+  const { getAccessTokenSilently } = useAuth0();
+  const queryClient = useQueryClient();
+
+  const { mutate, data, error, isPending, isSuccess } = useMutation({
+    mutationFn: async () => {
+      if (!boardId || !uuidValidate(boardId)) throw new Error('Invalid board ID.');
+
+      let token;
+      try {
+        token = await getAccessTokenSilently();
+      } catch (error) {
+        throw new Error('Failed to authenticate. Please try logging in again.');
+      }
+
+      try {
+        return deleteBoardById(boardId, token);
+      } catch (error) {
+        throw new Error('Failed to delete the board. Please try again.');
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['userBoardData'] });
+      queryClient.removeQueries({ queryKey: ['board', boardId] }); // Remove deleted board from cache
+    },
+  });
+
+  return { mutate, data, error, isPending, isSuccess };
+};
+
+export { useBoardById, useCreateBoard, useEditBoard, useDeleteBoard };
