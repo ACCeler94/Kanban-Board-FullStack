6c32e3eef7b2708e8f01d290d9e23e36a8664b65
Artur Celer <acceler945@gmail.com>
2024-12-09 17:38:59 +0100

    Delete unused validator and middleware

--- backend/validators/SubtaskIdSchema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-const SubtaskIdSchema = z.object({
-  subtaskId: z
-    .string({ message: 'Subtask ID is required' })
-    .uuid('Subtask ID must be a valid UUID'),
-});
-
-export default SubtaskIdSchema;
