f258cf529c6b829d2668cadd84a778275b18e514
Artur Celer <acceler945@gmail.com>
2025-01-29 20:17:31 +0100

    Add BoardForm tests

---
+++ client/tests/components/common/BoardForm.test.tsx
@@ -0,0 +1,62 @@
+import { render, screen } from '@testing-library/react';
+import BoardForm from '../../../src/components/common/BoardForm/BoardForm';
+import userEvent from '@testing-library/user-event';
+
+describe('BoardForm', () => {
+  const renderComponent = (boardTitle?: string) => {
+    const submitHandler = vi.fn();
+    render(
+      <BoardForm
+        submitHandler={submitHandler}
+        buttonText='submit'
+        boardTitle={boardTitle ? boardTitle : undefined}
+      />
+    );
+
+    return {
+      submitHandler,
+      user: userEvent.setup(),
+      titleInput: screen.getByRole('textbox'),
+      submitButton: screen.getByRole('button', { name: /submit/i }),
+    };
+  };
+
+  it('should render a form with title input and submit button', () => {
+    const { titleInput, submitButton } = renderComponent();
+    const label = screen.getByText(/board title/i);
+
+    expect(label).toBeInTheDocument();
+    expect(titleInput).toBeInTheDocument();
+    expect(titleInput).toHaveTextContent('');
+    expect(submitButton).toBeInTheDocument();
+  });
+
+  it('should call submitHandler with the value of title input', async () => {
+    const { submitHandler, titleInput, submitButton, user } = renderComponent();
+    const title = 'abc';
+
+    await user.type(titleInput, title);
+    await user.click(submitButton);
+
+    expect(submitHandler).toHaveBeenCalledWith(title);
+  });
+
+  it('should not call submitHandler if title is empty', async () => {
+    const { submitHandler, titleInput, submitButton, user } = renderComponent();
+
+    await user.click(submitButton);
+
+    expect(titleInput).toHaveAttribute('required');
+    expect(submitHandler).not.toHaveBeenCalled();
+  });
+
+  it('should call submitHandler with shortened title if user tried to type more than 100 characters', async () => {
+    const { submitHandler, titleInput, submitButton, user } = renderComponent();
+
+    await user.type(titleInput, 'a'.repeat(110));
+    await user.click(submitButton);
+
+    expect(titleInput).toHaveAttribute('MaxLength', '100');
+    expect(submitHandler).toHaveBeenCalledWith('a'.repeat(100));
+  });
+});
