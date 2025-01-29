b377a8155d9088750b20c161fea0008299c6b1a4
Artur Celer <acceler945@gmail.com>
2025-01-29 17:28:45 +0100

    Add additional packages, add vitest config and setup file

---
+++ client/tests/setup.ts
@@ -0,0 +1,14 @@
+import { PropsWithChildren, ReactNode } from 'react';
+import '@testing-library/jest-dom/vitest';
+
+vi.mock('@auth0/auth0-react', () => {
+  return {
+    useAuth0: vi.fn().mockReturnValue({
+      isAuthenticated: false,
+      isLoading: false,
+      user: undefined,
+    }),
+    Auth0Provider: ({ children }: PropsWithChildren) => children,
+    withAuthenticationRequired: (component: ReactNode) => component,
+  };
+});
