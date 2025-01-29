3234dad9a26d9faa78600ed6c029540034f45d10
Artur Celer <acceler945@gmail.com>
2025-01-29 17:06:23 +0100

    Refactor BoardLoader for better scalability

--- client/src/components/common/BoardLoader/BoardLoader.tsx
+++ client/src/components/common/BoardLoader/BoardLoader.tsx
@@ -2,26 +2,23 @@ import { Skeleton } from '@mui/material';
 import styles from './BoardLoader.module.css';
 
 const BoardLoader = () => {
+  const columns = [1, 2, 3]; // Three columns
+  const skeletonCount = 4; // Four skeletons per column
+
   return (
     <div className={styles.skeletonContainer}>
-      <div className={styles.columnWrapper}>
-        <Skeleton variant='rounded' sx={{ height: '20%', margin: '15px 0' }} />
-        <Skeleton variant='rounded' sx={{ height: '20%', margin: '15px 0' }} />
-        <Skeleton variant='rounded' sx={{ height: '20%', margin: '15px 0' }} />
-        <Skeleton variant='rounded' sx={{ height: '20%', margin: '15px 0' }} />
-      </div>
-      <div className={styles.columnWrapper}>
-        <Skeleton variant='rounded' sx={{ height: '20%', margin: '15px 0' }} />
-        <Skeleton variant='rounded' sx={{ height: '20%', margin: '15px 0' }} />
-        <Skeleton variant='rounded' sx={{ height: '20%', margin: '15px 0' }} />
-        <Skeleton variant='rounded' sx={{ height: '20%', margin: '15px 0' }} />
-      </div>
-      <div className={styles.columnWrapper}>
-        <Skeleton variant='rounded' sx={{ height: '20%', margin: '15px 0' }} />
-        <Skeleton variant='rounded' sx={{ height: '20%', margin: '15px 0' }} />
-        <Skeleton variant='rounded' sx={{ height: '20%', margin: '15px 0' }} />
-        <Skeleton variant='rounded' sx={{ height: '20%', margin: '15px 0' }} />
-      </div>
+      {columns.map((column) => (
+        <div key={column} className={styles.columnWrapper}>
+          {Array.from({ length: skeletonCount }).map((_, index) => (
+            <Skeleton
+              key={index}
+              variant='rounded'
+              aria-label='Loading skeleton'
+              sx={{ height: '20%', margin: '15px 0' }}
+            />
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
