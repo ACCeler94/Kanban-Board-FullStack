e0057094b4e890a7622dc4b3964e7774db53f334
Artur Celer <acceler945@gmail.com>
2025-01-29 17:29:19 +0100

    Add test for BoardLoader component

---
+++ client/tests/components/common/BoardLoader.test.tsx
@@ -0,0 +1,13 @@
+import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+import BoardLoader from '../../../src/components/common/BoardLoader/BoardLoader';
+
+describe('BoardLoader', () => {
+  it('should render skeletons', () => {
+    render(<BoardLoader />);
+
+    const skeletons = screen.getAllByLabelText(/skeleton/i);
+
+    expect(skeletons.length).toBeGreaterThan(0);
+  });
+});
