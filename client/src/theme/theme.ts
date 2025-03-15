19b8253e5b76351ad5f6a3c1ee6670e8eeda9715
Artur Celer <acceler945@gmail.com>
2025-03-15 13:57:26 +0100

    Remove dark theme from theme.ts

--- client/src/theme/theme.ts
+++ client/src/theme/theme.ts
@@ -1,7 +1,6 @@
 import '../styles/variables.css';
 import { createTheme } from '@mui/material/styles';
 
-// [TODO - fix css variables not working for colors]
 const lightTheme = createTheme({
   palette: {
     mode: 'light',
@@ -54,56 +53,4 @@ const lightTheme = createTheme({
   },
 });
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-    background: {
-      default: '#3F3F4E',
-    },
-    text: {
-      primary: 'var(--black)',
-    },
-    primary: {
-      main: '#635FC7',
-      contrastText: '#ffffff',
-    },
-    secondary: {
-      main: '#828FA3',
-      contrastText: '#635FC7',
-    },
-    error: {
-      main: '#EA5555',
-      contrastText: '#ffffff',
-    },
-    success: {
-      main: '#4caf50',
-      contrastText: '#ffffff',
-    },
-    warning: {
-      main: '#ff9800',
-      contrastText: '#ffffff',
-    },
-    info: {
-      main: '#2196f3',
-      contrastText: '#ffffff',
-    },
-  },
-  typography: {
-    fontFamily: 'var(--main-font)',
-    button: {
-      fontWeight: 800,
-    },
-  },
-  components: {
-    MuiButton: {
-      styleOverrides: {
-        root: {
-          borderRadius: '50px',
-          textTransform: 'none',
-        },
-      },
-    },
-  },
-});
-
-export { lightTheme, darkTheme };
+export { lightTheme };
