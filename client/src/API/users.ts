d0362e755d26456310e36111a6e3240157673f6e
Artur Celer <acceler945@gmail.com>
2025-03-19 13:07:39 +0100

    Delete unused useUserById query hook and corresponding fetchUserById function

--- client/src/API/users.ts
+++ client/src/API/users.ts
@@ -1,30 +1,10 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { UserBoardData, UserData, UserDataPreview } from '../types/types';
+import { UserBoardData, UserData } from '../types/types';
 import { apiUrl } from './config';
 
 // Actions
-const fetchUserById = async (userId: string): Promise<UserDataPreview> => {
-  if (typeof userId !== 'string' || !userId) {
-    // Check if id is not a string or is an empty string
-    throw new Error('Invalid User ID.');
-  }
-
-  try {
-    const { data } = await axios.get(`${apiUrl}/users/${userId}`);
-
-    return data;
-  } catch (error) {
-    if (axios.isAxiosError(error)) {
-      const errorMessage = error.response?.data?.error || 'An unexpected error occurred';
-      throw new Error(`Failed to fetch user data: ${error.response?.status} ${errorMessage}`);
-    } else {
-      throw new Error('An unexpected error occurred');
-    }
-  }
-};
-
 const fetchUserData = async (token: string): Promise<UserData | undefined> => {
   try {
     const { data } = await axios.get(`${apiUrl}/users/profile/`, {
@@ -64,21 +44,6 @@ const fetchUserBoardData = async (token: string): Promise<UserBoardData | undefi
 };
 
 // Hooks
-const useUserById = (userId: string) => {
-  const {
-    data: user,
-    error,
-    isPending,
-    refetch,
-  } = useQuery({
-    queryKey: ['user', userId],
-    queryFn: () => fetchUserById(userId),
-    enabled: !!userId,
-  });
-
-  return { user, error, isPending, refetch };
-};
-
 const useUserData = () => {
   const { getAccessTokenSilently, isAuthenticated } = useAuth0();
 
